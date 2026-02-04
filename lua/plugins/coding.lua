10077470ae05d8ab04571b17a6a2817329a12aaf
AndrewHerrada <andrew.herrada.villarroel@gmail.com>
2026-02-04 14:44:34 -0400

    Erased incline

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -5,23 +5,6 @@ return {
     cmd = "IncRename",
     config = true,
   },
-
-  -- Go forward/backward with square brackets
-  {
-    "nvim-mini/mini.bracketed",
-    event = "BufReadPost",
-    config = function()
-      local bracketed = require("mini.bracketed")
-      bracketed.setup({
-        file = { suffix = "" },
-        window = { suffix = "" },
-        quickfix = { suffix = "" },
-        yank = { suffix = "" },
-        treesitter = { suffix = "n" },
-      })
-    end,
-  },
-
   -- copilot
   {
     "zbirenbaum/copilot.lua",
