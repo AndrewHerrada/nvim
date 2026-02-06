10805bc2d9f1fe661a041f18fc458d74e0f0b09b
AndrewHerrada <andrew.herrada.villarroel@gmail.com>
2026-02-06 16:24:04 -0400

    Tokyo-night styles typegraph

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,13 @@ return {
   priority = 1000,
   opts = {
     style = "night",
-    transparent = true,
+    styles = {
+      functions = { bold = true },
+    },
+    on_highlights = function(hl, c)
+      hl["@keyword.conditional"] = { fg = c.cyan, italic = true }
+      hl["@keyword.repeat"] = { fg = c.cyan, italic = true }
+    end,
+    --transparent = true,
   },
 }
